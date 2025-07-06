4076902bd1335608056b77186f4e24156cbd9a1a
DanielJantjies <162917612+DanielJantjies@users.noreply.github.com>
2025-07-06 14:28:30 +0200

    Created Podcast Tile

---
+++ maybach music app/src/components/PodcastTile.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function PodcastTile ({podcast}) {
+
+    function onFavoriteClick () {
+        alert("clicked")
+    }
+
+    return  (
+
+    <div className='podcastContainer'>
+        <div className='podcastPoster'>
+            <img src={podcast.url}  alt={podcast.title}/>
+            <div className='posterOverlay'>
+                    <button className='favorite-btn' onClick={onFavoriteClick}>🖤</button>
+            </div>
+        </div>
+        <div className='podcastInformation'>
+            <h3 className='podcastTitle'>{podcast.title}</h3>
+            <h3 className='podcastSeason'>{podcast.season} {podcast.season === 1 ? 'Season' : 'Seasons'}</h3>
+            <p className='podcastDescription'>{podcast.description}</p>
+            <p className='podcastHosts'>{podcast.hosts.join("")}</p>
+            <p className='podcastGenre'>{podcast.genre.join("")}</p>
+
+
+        </div>
+    </div>
+
+    )
+}
+    
+
+export default PodcastTile
