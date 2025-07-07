9789d7a47380fd8593b54eeac8e107338706d667
DanielJantjies <162917612+DanielJantjies@users.noreply.github.com>
2025-07-07 16:15:59 +0200

    Initialise a Loading State

--- maybach music app/src/pages/Home.jsx
+++ maybach music app/src/pages/Home.jsx
@@ -1,34 +1,33 @@
 import PodcastTile from "../components/PodcastTile";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import fuzzysearch from "fuzzysearch";
 import { fetchAllPodcasts } from "../services/api";
-import { fetchAllPodcastsByGenre } from "../services/api";
-import { fetchPodcastById } from "../services/api";
-
 
 function Home() {
   const [searchQuery, setSearchQuery] = useState("");
-
   const [podcasts, setPodcasts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadPodcasts() {
-        const allPodcasts = await fetchAllPodcasts()
-        setPodcasts (allPodcasts)
-    } 
+      try {
+        const allPodcasts = await fetchAllPodcasts();
+        setPodcasts(allPodcasts);
+      } catch (err){
+        console.error("Failed to load Podcasts: " , err)
+      } finally{
+        setLoading(false);
+      }
+    }
 
     loadPodcasts();
-
-  },[]) ;
-
-
+  }, []);
 
   const handleSearch = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     alert(`Searching for: ${searchQuery}`);
     setSearchQuery("Successfully searched for: " + searchQuery);
   };
-   
 
   return (
     <div className="home">
@@ -44,15 +43,24 @@ function Home() {
           Search
         </button>
       </form>
-      <div className="podcastGrid">
-        {podcasts.filter((currentPodcast) => fuzzysearch(searchQuery.toLowerCase(), currentPodcast.title.toLowerCase()))
-        
-        .map((currentPodcast) => 
-            
-        (
-          <PodcastTile key={currentPodcast.id} podcast={currentPodcast} />
-        ))}
-      </div>
+
+        {loading ? (
+            <div className="loading">Loading...</div>
+        ):(<div className="podcastGrid">
+        {podcasts
+          .filter((currentPodcast) =>
+            fuzzysearch(
+              searchQuery.toLowerCase(),
+              currentPodcast.title.toLowerCase()
+            )
+          )
+
+          .map((currentPodcast) => (
+            <PodcastTile key={currentPodcast.id} podcast={currentPodcast} />
+          ))}
+      </div>)}
+
+      
     </div>
   );
 }
