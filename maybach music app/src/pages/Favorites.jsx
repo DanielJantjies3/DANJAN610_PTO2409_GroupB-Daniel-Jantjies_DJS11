06611dd65b84a5e9c4fad6a0ecab2e297c09207f
DanielJantjies <162917612+DanielJantjies@users.noreply.github.com>
2025-07-07 18:05:08 +0200

    Created Favorites Page Display

--- maybach music app/src/pages/Favorites.jsx
+++ maybach music app/src/pages/Favorites.jsx
@@ -1,4 +1,56 @@
+import { usePodcastContext } from "../contexts/PodcastContext";
+import { useState, useEffect } from "react";
+import fuzzysearch from "fuzzysearch";
+import PodcastTile from "../components/PodcastTile";
+
 function Favorites() {
+
+    const [searchQuery, setSearchQuery] = useState("");
+    const [loading, setLoading] = useState(true);
+
+    const {favorites} = usePodcastContext ();
+
+      const handleSearch = (e) => {
+    e.preventDefault();
+    alert(`Searching for: ${searchQuery}`);
+    setSearchQuery("Successfully searched for: " + searchQuery);
+  };
+
+
+    if (favorites)   return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="searchForm">
+        <input
+          type="text"
+          placeholder="Search Podcasts..."
+          className="searchInput"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="searchButton">
+          Search
+        </button>
+      </form>
+
+        <div className="podcastGrid">
+        {favorites
+          .filter((currentPodcast) =>
+            fuzzysearch(
+              searchQuery.toLowerCase(),
+              currentPodcast.title.toLowerCase()
+            )
+          )
+
+          .map((currentPodcast) => (
+            <PodcastTile key={currentPodcast.id} podcast={currentPodcast} />
+          ))}
+      </div>
+
+      
+    </div>
+  );
+        
+
     return (
         <div>
             <h1>Your Favorites Page</h1>
